3e9b4464c38b4dd67d56c1d9350e5d7a74e8e8d1
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2020-08-08 16:12:42 -0400

    Setup kotest & mockk

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,32 +1,54 @@
-plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-    // Apply the application plugin to add support for building a CLI application.
+plugins {
+    kotlin("jvm").version("1.3.72")
     application
+    jacoco
+    id("org.jlleitschuh.gradle.ktlint").version("9.2.1")
+    id("com.github.ben-manes.versions").version("0.28.0")
 }
 
 repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
     jcenter()
 }
 
 dependencies {
-    // Align versions of all Kotlin components
+    val log4jVersion = "2.13.1"
+    val vertxVersion = "3.9.0"
+    val kotestVersion = "4.1.3"
+
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
+    implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
+    implementation("io.vertx:vertx-web:$vertxVersion")
+    implementation("io.vertx:vertx-web-client:$vertxVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
+    implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
+    
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("io.vertx:vertx-web-client:$vertxVersion")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
 }
 
 application {
-    // Define the main class for the application.
     mainClassName = "info.ditrapani.AppKt"
+}
+
+ktlint {
+    version.set("0.36.0")
+    enableExperimentalRules.set(true)
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.test {
+    testLogging {
+        events("passed", "started", "failed", "skipped")
+    }
+    useJUnitPlatform()
 }
