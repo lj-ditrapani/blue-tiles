1ff6c6b2aa61a34b90522a22f74656f2c770ac93
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2020-08-08 16:46:05 -0400

    Hello world server

--- backend/src/main/kotlin/info/ditrapani/App.kt
+++ backend/src/main/kotlin/info/ditrapani/App.kt
@@ -1,9 +1,27 @@
 package info.ditrapani
 
-class Server {
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+import io.vertx.kotlin.core.http.listenAwait
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+
+private const val PORT = 44777
+
+class Server : CoroutineVerticle() {
     fun hi(): String = "hello"
+
+    override suspend fun start() {
+        val router = Router.router(vertx)
+        router.get("/hello").handler { routingContext ->
+            routingContext.response().end("hello!!!")
+        }
+        val server = vertx.createHttpServer().requestHandler(router)
+        server.listenAwait(PORT)
+        println("Test server listening on port $PORT")
+    }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     println(Server().hi())
+    Vertx.vertx().deployVerticle(Server())
 }
