7009408b6fa1a9daa95ef68b8b9c92c5556864c7
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2020-08-15 13:16:36 -0400

    Add initial Game type and parse some fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,28 @@ type AppProps = {
   hi: String
 }
 
+type Game = {
+  requestingPerson: Player | null
+  currentFirstPlayer: Player
+  currentPlayer: Player
+  // "factory" to factory.toJson(),
+  supplyCount: number
+  trashCount: number
+  /*
+  "board1" to board1.toJson(),
+  "board2" to board1.toJson(),
+  "board3" to board1.toJson(),
+  "lastPlay" to lastPlay.toJson(),
+  */
+  winner: Player | null
+}
+
 type AppState = {
-  registered: Boolean
-  ready: Boolean
+  registered: boolean
+  ready: boolean
   player: Player | undefined
   playerCount: number
-  game: any
+  game: Game | null
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -68,7 +84,16 @@ class App extends React.Component<AppProps, AppState> {
     const body = await result.json()
     console.log('called status:')
     console.log(body)
-    // this.setState({ game: body })
+    this.setState({
+      game: {
+        requestingPerson: parsePlayerOrNull(body.requestingPerson),
+        currentFirstPlayer: parsePlayer(body.currentFirstPlayer),
+        currentPlayer: parsePlayer(body.currentPlayer),
+        supplyCount: body.supplyCount,
+        trashCount: body.trashCount,
+        winner: parsePlayerOrNull(body.winner),
+      },
+    })
   }
 
   render() {
@@ -76,11 +101,11 @@ class App extends React.Component<AppProps, AppState> {
       return (
         <div className="App">
           <p> Registered </p>
-          <p> is ready? {this.state.ready} </p>
+          <p> is ready? {String(this.state.ready)} </p>
           <p> waiting for {3 - this.state.playerCount} players to join </p>
           <p> this.props.hi </p>
           <p> You are player {this.state.player} </p>
-          <p> Game is {this.state.game} </p>
+          {this.renderGame(this.state.game)}
         </div>
       )
     } else {
@@ -91,8 +116,46 @@ class App extends React.Component<AppProps, AppState> {
       )
     }
   }
+
+  renderGame(game: Game | null) {
+    if (game === null) {
+      return <p>No game state yet</p>
+    } else {
+      return (
+        <div>
+          <p>requestingPerson: {game.requestingPerson}</p>
+          <p>currentFirstPlayer: {game.currentFirstPlayer}</p>
+          <p>currentPlayer: {game.currentPlayer}</p>
+          <p>supplyCount: {game.supplyCount}</p>
+          <p>trashCount: {game.trashCount}</p>
+          <p>winner: {String(game.winner)}</p>
+        </div>
+      )
+    }
+  }
 }
 
 type Player = 'P1' | 'P2' | 'P3'
 
 export default App
+
+const parsePlayer = (str: string | null | undefined): Player => {
+  const p = parsePlayerOrNull(str)
+  if (p === null) {
+    throw Error('Player is null')
+  } else {
+    return p
+  }
+}
+
+const parsePlayerOrNull = (str: string | null | undefined): Player | null => {
+  if (str === 'P1') {
+    return 'P1'
+  } else if (str === 'P2') {
+    return 'P2'
+  } else if (str === 'P3') {
+    return 'P3'
+  } else {
+    return null
+  }
+}
