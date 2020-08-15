211fcbf99d1215a33e9c7e1e8c997da1a97d8d55
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2020-08-15 11:22:05 -0400

    Call register and over loop ready
    Display results.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,10 @@ type AppProps = {
 }
 
 type AppState = {
-  registered: Boolean,
+  registered: Boolean
   ready: Boolean
+  player: Player | undefined
+  playerCount: number
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -16,6 +18,45 @@ class App extends React.Component<AppProps, AppState> {
     this.state = {
       registered: false,
       ready: false,
+      player: undefined,
+      playerCount: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.setup()
+  }
+
+  async setup() {
+    await this.registerUser()
+    await this.waitForReady()
+  }
+
+  async registerUser() {
+    const result = await fetch('/register', { method: 'POST' })
+    const body = await result.json()
+    console.log('register success')
+    console.log(body)
+    if (body.status == 'registered') {
+      this.setState({
+        player: body.player,
+        registered: true,
+      })
+    }
+  }
+
+  async waitForReady() {
+    const result = await fetch('/ready')
+    const body = await result.json()
+    console.log(body)
+    if (body.ready) {
+      this.setState({
+        ready: true,
+        playerCount: body.count,
+      })
+    } else {
+      this.setState({ playerCount: body.count })
+      setTimeout(() => this.waitForReady(), 500)
     }
   }
 
@@ -24,7 +65,10 @@ class App extends React.Component<AppProps, AppState> {
       return (
         <div className="App">
           <p> Registered </p>
+          <p> is ready? {this.state.ready} </p>
+          <p> waiting for {3 - this.state.playerCount} players to join </p>
           <p> this.props.hi </p>
+          <p> You are player {this.state.player} </p>
         </div>
       )
     } else {
@@ -36,5 +80,7 @@ class App extends React.Component<AppProps, AppState> {
     }
   }
 }
+
+type Player = 'P1' | 'P2' | 'P3'
 
 export default App
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App hi='hello' />
+    <App hi="hello" />
   </React.StrictMode>,
   document.getElementById('root')
 )
